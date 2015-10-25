a8849b001cd425d465e43ca6a961dd274038bb58
Greg Look <greg@greg-look.net>
2015-10-25 00:57:18 -0700

    Update dependencies.

--- project.clj
+++ project.clj
@@ -8,9 +8,11 @@
   [[lein-protobuf "0.4.3"]]
 
   :dependencies
-  [[clj-time "0.11.0"]
+  [[byte-streams "0.1.13"]
+   [clj-time "0.11.0"]
    [mvxcvi/blobble "0.1.0-SNAPSHOT"]
    [mvxcvi/multihash "0.3.0-SNAPSHOT"]
+   [mvxcvi/puget "0.9.2"]
    [org.clojure/clojure "1.7.0"]
    [org.clojure/tools.logging "0.3.1"]
    [org.flatland/protobuf "0.8.1"]])
