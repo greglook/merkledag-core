f032a805571ba8ecda2b1bfdda5746d82a83e389
Greg Look <greg@greg-look.net>
2015-10-29 19:17:52 -0700

    Upgrade to puget 1.0.0.

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
    [clj-time "0.11.0"]
    [mvxcvi/blobble "0.1.0-SNAPSHOT"]
    [mvxcvi/multihash "0.2.0"]
-   [mvxcvi/puget "0.9.2"]
+   [mvxcvi/puget "1.0.0"]
    [org.clojure/clojure "1.7.0"]
    [org.clojure/tools.logging "0.3.1"]
    [org.flatland/protobuf "0.8.1"]]
