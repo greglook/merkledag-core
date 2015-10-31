fd4884c61fb1162146974ac5deaaf37b943fd29b
Greg Look <greg@greg-look.net>
2015-10-30 20:02:52 -0700

    Update graph test.

--- test/merkledag/graph_test.clj
+++ test/merkledag/graph_test.clj
@@ -18,12 +18,12 @@
   {:print-handlers
    (dispatch/chained-lookup
      {Multihash (puget/tagged-handler 'data/hash multihash/base58)
-      MerkleLink (puget/tagged-handler 'data/link :name #_(juxt :name :target :tsize))}
+      MerkleLink (puget/tagged-handler 'data/link (juxt :name :target :tsize))}
      puget/common-handlers)})
 
 
 (def hash-1 (multihash/decode "Qmb2TGZBNWDuWsJVxX7MBQjvtB3cUc4aFQqrST32iASnEh"))
-(def hash-2 (multihash/decode "Qmb2TGZBNWDuWsJVxX7MBQjvtB3cUc4aFQqrST32iASnEh"))
+(def hash-2 (multihash/decode "Qmd8kgzaFLGYtTS1zfF37qKGgYQd5yKcQMyBeSa8UkUz4W"))
 
 
 (deftest a-test
@@ -31,7 +31,7 @@
     (merkle/with-repo {:types core-types, :store store}
       (testing "basic node properties"
         (let [node (merkle/node
-                     [(merkle/link "@context" context-hash)]
+                     [(merkle/link "@context" hash-1)]
                      {:type :finance/posting
                       :uuid "foo-bar"})]
           (is (instance? Multihash (:id node)))
@@ -42,20 +42,24 @@
           (is (map? (:data node)))
           (is (empty? (blob/list store))
               "node creation should not store any blobs")))
-      (let [node-1 (merkle/node
-                     {:type :finance/posting
-                      :uuid "foo-bar"})
-            node-2 (merkle/node
-                     {:type :finance/posting
-                      :uuid "baz-qux"})
-            node-3 (merkle/node
-                     [(merkle/link "@context" context-hash)]
-                     {:type :finance/transaction
-                      :title "SCHZ - Reinvest Dividend"
-                      :description "Automatic dividend reinvestment."
-                      :time #inst "2013-10-08T00:00:00Z"
-                      :entries [(merkle/link "posting-1" node-1)
-                                (merkle/link "posting-2" node-2)]})]
-        ;(puget/cprint node-3 print-options)
-        ;(bytes/print-bytes (:content node-3))
-        ))))
+      (testing "multi-node reference"
+        (let [node-1 (merkle/node
+                       {:type :finance/posting
+                        :uuid "foo-bar"})
+              node-2 (merkle/node
+                       {:type :finance/posting
+                        :uuid "frobblenitz omnibus"})
+              node-3 (merkle/node
+                       [(merkle/link "@context" hash-1)]
+                       {:type :finance/transaction
+                        :uuid #uuid "31f7dd72-c7f7-4a15-a98b-0f9248d3aaa6"
+                        :title "SCHZ - Reinvest Dividend"
+                        :description "Automatic dividend reinvestment."
+                        :time #inst "2013-10-08T00:00:00Z"
+                        :entries [(merkle/link "posting-1" node-1)
+                                  (merkle/link "posting-2" node-2)]})]
+          ;(puget/cprint node-3 print-options)
+          ;(bytes/print-bytes (:content node-3))
+          (is (= 3 (count (:links node-3))))
+          (is (every? (partial instance? MerkleLink) (:links node-3)))
+          )))))
