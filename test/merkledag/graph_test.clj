9f67527f40c9dc0b218714bf75e2d116022bf701
Greg Look <greg@greg-look.net>
2015-11-25 16:20:21 -0800

    Update repl config and graph tests.

--- test/merkledag/graph_test.clj
+++ test/merkledag/graph_test.clj
@@ -1,13 +1,15 @@
 (ns merkledag.graph-test
   (:require
-    [blobble.core :as blob]
-    [blobble.store.memory :refer [memory-store]]
+    [blocks.core :as block]
+    [blocks.store.memory :refer [memory-store]]
     [byte-streams :as bytes :refer [bytes=]]
     [clj-time.core :as time]
     [clojure.test :refer :all]
-    [merkledag.data :as data]
-    [merkledag.graph :as merkle]
-    [merkledag.test-utils :refer [dprint]]
+    (merkledag
+      [data :as data]
+      [core :as merkle]
+      [graph :as graph]
+      [test-utils :refer [dprint]])
     [multihash.core :as multihash])
   (:import
     merkledag.link.MerkleLink
@@ -19,22 +21,20 @@
 
 
 (deftest a-test
-  (let [store (memory-store)
-        repo (merkle/graph-repo store)]
-    (binding []
-      (testing "basic node properties"
-        (let [node (merkle/node
-                     [(merkle/link "@context" hash-1)]
-                     {:type :finance/posting
-                      :uuid "foo-bar"})]
-          (is (instance? Multihash (:id node)))
-          (is (instance? java.nio.ByteBuffer (:content node)))
-          (is (vector? (:links node)))
-          (is (= 1 (count (:links node))))
-          (is (every? (partial instance? MerkleLink) (:links node)))
-          (is (map? (:data node)))
-          (is (empty? (blob/list store))
-              "node creation should not store any blobs")))
+  (let [repo (graph/block-graph)]
+    (testing "basic node properties"
+      (let [node (merkle/node (:format repo)
+                   [(merkle/link "@context" hash-1)]
+                   {:type :finance/posting
+                    :uuid "foo-bar"})]
+        (is (instance? Multihash (:id node)))
+        (is (vector? (:links node)))
+        (is (= 1 (count (:links node))))
+        (is (every? (partial instance? MerkleLink) (:links node)))
+        (is (map? (:data node)))
+        (is (empty? (block/list repo))
+            "node creation should not store any blocks")))
+    (graph/with-context repo
       (testing "multi-node reference"
         (let [node-1 (merkle/node
                        {:type :finance/posting
@@ -51,7 +51,7 @@
                         :time (data/parse-inst "2013-10-08T00:00:00")
                         :entries [(merkle/link "posting-1" node-1)
                                   (merkle/link "posting-2" node-2)]})]
-          ;(bytes/print-bytes (:content node-3))
+          ;(bytes/print-bytes (block/open node-3))
           (is (= 3 (count (:links node-3))))
           (is (every? (partial instance? MerkleLink) (:links node-3)))
           (merkle/put-node! repo node-1)
@@ -59,10 +59,9 @@
           (merkle/put-node! repo node-3)
           (let [node' (merkle/get-node repo (:id node-3))]
             (is (= (:id node') (:id node-3)))
-            (is (bytes= (:content node') (:content node-3)))
+            (is (bytes= (block/open node') (block/open node-3)))
             (is (= (:links node') (:links node-3)))
-            (is (= (:data node') (:data node-3))))
-          )))))
+            (is (= (:data node') (:data node-3)))))))))
 
 
 ; TODO: test raw :data segments
