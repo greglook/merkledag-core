122d725e65912493f258cd59ab0c96e07d075e2e
Greg Look <greg@greg-look.net>
2016-04-30 13:03:32 -0700

    Initial rewrite of core tests.

--- test/merkledag/core_test.clj
+++ test/merkledag/core_test.clj
@@ -2,65 +2,102 @@
   (:require
     [blocks.core :as block]
     [blocks.store.memory :refer [memory-store]]
-    [byte-streams :as bytes :refer [bytes=]]
-    [clj-time.core :as time]
     [clojure.test :refer :all]
-    (merkledag
-      [core :as merkle]
-      [test-utils :refer [dprint]])
-    [multihash.core :as multihash])
+    [merkledag.codecs.edn :as edn]
+    [merkledag.core :as merkle]
+    [merkledag.data :as data]
+    [merkledag.refs.memory :refer [memory-tracker]]
+    [multihash.core :as multihash]
+    [puget.dispatch :as dispatch]
+    [puget.printer :as puget])
   (:import
+    blocks.data.Block
     merkledag.link.MerkleLink
     multihash.core.Multihash))
+
+
+(def dprint-opts
+  {:print-color true
+   :print-handlers
+   (dispatch/chained-lookup
+     {MerkleLink (puget/tagged-handler 'data/link (juxt :name :target :tsize))
+      Multihash (puget/tagged-handler 'data/hash multihash/base58)}
+     (edn/types->print-handlers data/core-types)
+     puget/common-handlers)})
+
+
+(defmacro dprint
+  [v]
+  `(do
+     (print "DEBUG: ")
+     (puget/pprint (quote ~v) dprint-opts)
+     (puget/pprint ~v dprint-opts)))
 
 
 (def hash-1 (multihash/decode "Qmb2TGZBNWDuWsJVxX7MBQjvtB3cUc4aFQqrST32iASnEh"))
 (def hash-2 (multihash/decode "Qmd8kgzaFLGYtTS1zfF37qKGgYQd5yKcQMyBeSa8UkUz4W"))
 
 
-#_
-(deftest a-test
-  (let [repo (graph/block-graph)]
-    (testing "basic node properties"
-      (let [node (merkle/node (:format repo)
-                   [(merkle/link "@context" hash-1)]
-                   {:type :finance/posting
-                    :uuid "foo-bar"})]
-        (is (instance? Multihash (:id node)))
+(defn make-test-repo
+  []
+  (merkle/graph-repo
+    :store (memory-store)
+    :refs (memory-tracker)))
+
+
+(deftest node-construction
+  (let [codec (:codec (make-test-repo))]
+    (testing "with no data"
+      (is (nil? (merkle/node* codec nil))))
+    (testing "with only data"
+      (let [node (merkle/node* codec
+                   {:data/type :bar})]
+        (is (instance? Block node))
+        (is (nil? (:links node)))
+        (is (= {:data/type :bar} (:data node)))))
+    (testing "with only links"
+      (let [node (merkle/node* codec
+                   [(merkle/link* "@context" hash-1)]
+                   nil)]
+        (is (instance? Block node))
         (is (vector? (:links node)))
         (is (= 1 (count (:links node))))
         (is (every? (partial instance? MerkleLink) (:links node)))
-        (is (map? (:data node)))
-        (is (empty? (block/list repo))
-            "node creation should not store any blocks")))
-    (graph/with-context repo
-      (testing "multi-node reference"
-        (let [node-1 (merkle/node
-                       {:type :finance/posting
-                        :uuid "foo-bar"})
-              node-2 (merkle/node
-                       {:type :finance/posting
-                        :uuid "frobblenitz omnibus"})
-              node-3 (merkle/node
-                       [(merkle/link "@context" hash-1)]
-                       {:type :finance/transaction
-                        :uuid #uuid "31f7dd72-c7f7-4a15-a98b-0f9248d3aaa6"
-                        :title "SCHZ - Reinvest Dividend"
-                        :description "Automatic dividend reinvestment."
-                        :time #inst "2013-10-08T00:00:00"
-                        :entries [(merkle/link "posting-1" node-1)
-                                  (merkle/link "posting-2" node-2)]})]
-          ;(bytes/print-bytes (block/open node-3))
-          (is (= 3 (count (:links node-3))))
-          (is (every? (partial instance? MerkleLink) (:links node-3)))
-          (merkle/put-node! repo node-1)
-          (merkle/put-node! repo node-2)
-          (merkle/put-node! repo node-3)
-          (let [node' (merkle/get-node repo (:id node-3))]
-            (is (= (:id node') (:id node-3)))
-            (is (bytes= (block/open node') (block/open node-3)))
-            (is (= (:links node') (:links node-3)))
-            (is (= (:data node') (:data node-3)))))))))
-
-
-; TODO: test raw :data segments
+        (is (= "@context" (get-in node [:links 0 :name])))
+        (is (nil? (:data node)))))
+    (testing "with data and table-only links"
+      (let [node (merkle/node* codec
+                   [(merkle/link* "@context" hash-1)]
+                   {:data/type :abc/xyz
+                    :data/ident "foo-bar"})]
+        (is (instance? Block node))
+        (is (vector? (:links node)))
+        (is (= 1 (count (:links node))))
+        (is (every? (partial instance? MerkleLink) (:links node)))
+        (is (= "@context" (get-in node [:links 0 :name])))
+        (is (= :abc/xyz (get-in node [:data :data/type])))))
+    (testing "with links in data"
+      (let [foo-block (block/read! "foo thing")
+            node (merkle/node* codec
+                   nil
+                   {:data/type :abc/xyz
+                    :thing (merkle/link* "foo" foo-block)})]
+        (is (instance? Block node))
+        (is (= 1 (count (:links node))))
+        (is (every? (partial instance? MerkleLink) (:links node)))
+        (is (= ["foo"] (map :name (:links node))))
+        (is (= [(:id foo-block)] (map :target (:links node))))
+        (is (= :abc/xyz (get-in node [:data :data/type])))))
+    (testing "with data and links"
+      (let [foo-block (block/read! "foo thing")
+            node (merkle/node* codec
+                   [(merkle/link* "qux" hash-1)
+                    (merkle/link* "foo" foo-block)]
+                   {:data/type :abc/xyz
+                    :thing (merkle/link* "foo" foo-block)})]
+        (is (instance? Block node))
+        (is (= 2 (count (:links node))))
+        (is (every? (partial instance? MerkleLink) (:links node)))
+        (is (= ["qux" "foo"] (map :name (:links node))))
+        (is (= [hash-1 (:id foo-block)] (map :target (:links node))))
+        (is (= (second (:links node)) (get-in node [:data :thing])))))))
