962825dfcda28df4077cabd2c06dad60e81b082b
Greg Look <greg@greg-look.net>
2016-04-29 20:15:31 -0700

    Update project dependencies.

--- project.clj
+++ project.clj
@@ -8,15 +8,22 @@
 
   :dependencies
   [[bultitude "0.2.8"]
-   [byte-streams "0.2.0"]
+   [byte-streams "0.2.2"]
    [clj-time "0.11.0"]
    [mvxcvi/blocks "0.6.1"]
    [mvxcvi/multicodec "0.5.1"]
-   [mvxcvi/multihash "1.1.0"]
+   [mvxcvi/multihash "2.0.0"]
    [mvxcvi/puget "1.0.0"]
    [org.clojure/clojure "1.8.0"]
    [org.clojure/tools.logging "0.3.1"]
-   [prismatic/schema "1.0.5"]]
+   [prismatic/schema "1.1.0"]]
+
+  :aliases
+  {"coverage" ["with-profile" "+test,+coverage" "cloverage"]}
+
+  :test-selectors
+  {:unit (complement :integration)
+   :integration :integration}
 
   :hiera
   {:vertical false
@@ -34,4 +41,9 @@
   :profiles
   {:repl {:source-paths ["dev"]
           :dependencies [[org.clojure/tools.namespace "0.2.11"]
-                         [rhizome "0.2.5"]]}})
+                         [rhizome "0.2.5"]]}
+   :test {:dependencies [[commons-logging "1.2"]]
+          :jvm-opts ["-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.NoOpLog"]}
+   :coverage {:plugins [[lein-cloverage "1.0.6"]]
+              :jvm-opts ["-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
+                         "-Dorg.apache.commons.logging.simplelog.defaultlog=trace"]}})
