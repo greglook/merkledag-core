2ab5479265a37ca531f6fe9eda0437ada28c0e9c
Greg Look <greg@greg-look.net>
2017-05-19 00:38:18 -0700

    Add NodeStore protocol.

--- src/merkledag/node.clj
+++ src/merkledag/node.clj
@@ -52,3 +52,130 @@
     :reader link/link-index
     :writers {LinkIndex :index}
     :cbor/tag 72}})
+
+
+
+;; ## Identity Protocol
+
+(defprotocol Identifiable
+  "Protocol for values which can be resolved to a multihash value in order to
+  uniquely identify a node in the graph."
+
+  (identify
+    [value]
+    "Return the multihash identifying the value."))
+
+
+(extend-protocol Identifiable
+
+  nil
+  (identify [_] nil)
+
+  multihash.core.Multihash
+  (identify [m] m)
+
+  blocks.data.Block
+  (identify [b] (:id b))
+
+  merkledag.link.MerkleLink
+  (identify [l] (:target l)))
+
+
+
+;; ## Node Storage API
+
+(defprotocol NodeStore
+  "Node stores provide an interface for creating, persisting, and retrieving
+  node data."
+
+  (-get-node
+    [store id]
+    "Retrieve a node map from the store by id.")
+
+  (-store-node!
+    [store links data]
+    "Create a new node by serializing the links and data. Returns a new node
+    map.")
+
+  (-delete-node!
+    [store id]
+    "Remove a node from the store."))
+
+; TODO: flush?
+
+
+(defn get-node
+  "Retrieve the identified node from the store. Returns a node map.
+
+  The id may be any `Identifiable` value."
+  [store id]
+  (when-let [id (identify id)]
+    (-get-node store id)))
+
+
+(defn get-links
+  "Retrieve the links for the identified node. Returns the node's link table as
+  a vector of `MerkleLink` values.
+
+  The id may be any `Identifiable` value."
+  [store id]
+  (let [id (identify id)]
+    (when-let [node (and id (-get-node store id))]
+      (some->
+        (:links node)
+        (vary-meta
+          assoc
+          ::id id
+          ::size (::size node))))))
+
+
+(defn get-data
+  "Retrieve the data for the identified node. Returns the node's data body.
+
+  The id may be any `Identifiable` value."
+  [store id]
+  (let [id (identify id)]
+    (when-let [node (and id (-get-node store id))]
+      (some->
+        (:data node)
+        (vary-meta
+          assoc
+          ::id id
+          ::size (::size node)
+          ::links (:links node))))))
+
+
+(defn store-node!
+  "Create a new node by serializing the links and data. Returns the stored node
+  map, or nil if both links and data are nil."
+  ([store data]
+   (store-node! store nil data))
+  ([store links data]
+   (when (or links data)
+     (-store-node! store links data))))
+
+
+(defn delete-node!
+  "Remove a node from the store.
+
+  The id may be any `Identifiable` value."
+  [store id]
+  (when-let [id (identify id)]
+    (-delete-node! store id)))
+
+
+
+;; ## Node Utilities
+
+(defn total-size
+  "Calculates the total size of data reachable from the given node. Expects a
+  node map with `::size` and `::links` entries.
+
+  Raw blocks and nodes with no links have a total size equal to their `::size`.
+  Each link in the node's link table adds its `::link/tsize` to the total.
+  Returns `nil` if no node is given."
+  [node]
+  (when-let [size (::size node)]
+    (->> (::links node)
+         (keep ::link/tsize)
+         (reduce + size))))
