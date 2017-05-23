d487a71b76b04f56a82e5522d4cad27eadca9428
Greg Look <greg@greg-look.net>
2017-05-22 23:12:09 -0700

    Separate node codec back out into separate namespace.

---
+++ src/merkledag/codec/node.clj
@@ -0,0 +1,103 @@
+(ns merkledag.codec.node
+  (:require
+    [merkledag.codec.cbor :refer [cbor-codec]]
+    [merkledag.codec.edn :refer [edn-codec]]
+    [merkledag.link :as link]
+    [merkledag.node :as node]
+    [multicodec.core :as codec]
+    [multicodec.header :as header]
+    [multicodec.codecs.mux :refer [mux-codec]]
+    [multihash.core :as multihash])
+  (:import
+    merkledag.link.LinkIndex
+    merkledag.link.MerkleLink
+    multihash.core.Multihash))
+
+
+(def core-types
+  "The core type definitions for hashes and links which are used in the base
+  merkledag data structure."
+  {'data/hash
+   {:description "Content-addressed multihash references"
+    :reader multihash/decode
+    :cbor/tag 68
+    :cbor/writers {Multihash multihash/encode}
+    :edn/writers {Multihash multihash/base58}}
+
+   'data/link
+   {:description "Merkle link values"
+    :reader link/form->link
+    :writers {MerkleLink link/link->form}
+    :cbor/tag 69}
+
+   'data/link-index
+   {:description "Indexes to the link table within a node"
+    :reader link/link-index
+    :writers {LinkIndex :index}
+    :cbor/tag 72}})
+
+
+
+;; ## Node Codec
+
+(defrecord NodeCodec
+  [header mux]
+
+  codec/Encoder
+
+  (encodable?
+    [this value]
+    (boolean (and (map? value)
+                  (or (seq (::links value))
+                      (::data value)))))
+
+
+  (encode!
+    [this output node]
+    (when-not (codec/encodable? this node)
+      (throw (ex-info (str "Cannot encode node data: " (pr-str node))
+                      {:node node})))
+    (let [links' (when (seq (::links node)) (vec (::links node)))
+          data' (link/replace-links links' (::data node))]
+      (codec/encode! mux output [links' data'])))
+
+
+  codec/Decoder
+
+  (decodable?
+    [this header']
+    (= header header'))
+
+
+  (decode!
+    [this input]
+    (let [[links data :as value] (codec/decode! mux input)]
+      (when-not (and (vector? value) (or links data))
+        (throw (ex-info "Decoded bad node value missing links and data"
+                        {:value value})))
+      {::links links
+       ::data (link/resolve-indexes links data)})))
+
+
+;; Privatize automatic constructor functions.
+(alter-meta! #'->NodeCodec assoc :private true)
+(alter-meta! #'map->NodeCodec assoc :private true)
+
+
+(defn node-codec
+  "Construct a new node codec. The codec will serialize values using the given
+  map of types, merged into `core-types`."
+  [types]
+  (let [types+ (merge core-types types)
+        edn (edn-codec types+)
+        cbor (cbor-codec types+)
+        data-mux (mux-codec :cbor cbor :edn edn)]
+    (->NodeCodec
+      "/merkledag/v1"
+      data-mux
+      #_ ; TODO: support compression wrapers
+      (mux-codec
+        :snappy (snappy-codec data-mux)
+        :gzip (gzip-codec data-mux)
+        :cbor cbor
+        :edn edn))))
--- src/merkledag/store/block.clj
+++ src/merkledag/store/block.clj
@@ -5,8 +5,75 @@
     [clojure.core.cache :as cache]
     [merkledag.node :as node]
     [merkledag.store.cache :as msc]
-    [merkledag.store.core :as store]))
+    [merkledag.store.core :as store]
+    [multicodec.core :as codec]
+    [multicodec.header :as header]
+    [multicodec.codecs.mux :refer [mux-codec]]
+    [multihash.core :as multihash])
+  (:import
+    java.io.ByteArrayInputStream))
 
+
+;; ## Formatting Functions
+
+(defn- decode-info!
+  [codec input]
+  (binding [header/*headers* []]
+    (try
+      (-> (codec/decode! codec input)
+          (select-keys [::links ::data])
+          (assoc ::encoding header/*headers*))
+      (catch clojure.lang.ExceptionInfo ex
+        (case (:type (ex-data ex))
+          :multicodec/bad-header
+            {::encoding nil}
+          :multicodec.codecs.mux/no-codec
+            {::encoding header/*headers*}
+          (throw ex))))))
+
+
+(defn format-block
+  "Serializes the given data value into a block using the codec. Returns a
+  block containing the formatted content and extra node attributes."
+  [codec value]
+  (when value
+    (if (codec/encodable? codec value)
+      (binding [header/*headers* []]
+        (let [content (codec/encode codec value)  ; TODO: encode-with-header
+              encoded-headers header/*headers*
+              block (block/read! content)
+              info (decode-info! codec (ByteArrayInputStream. content))]
+          (when-not (= encoded-headers (:encoding info))
+            (throw (ex-info "Decoded headers do not match written encoding"
+                            {:encoded encoded-headers
+                             :decoded (:encoding info)})))
+          (into block info)))
+      (try
+        (assoc (block/read! value)
+               ::encoding nil)
+        (catch Exception ex
+          (throw (ex-info "Value is not valid node data and can't be read as raw bytes"
+                          {:value value}
+                          ex)))))))
+
+
+(defn parse-block
+  "Attempts to parse the contents of a block with the given codec. Returns an
+  updated version of the block with additional keys set. At a minimum, this
+  will add an `::encoding` key with the detected codec, or `nil` for raw
+  blocks.
+
+  The dispatched codec should return a map of attributes to merge into the
+  block; typically including a `::data` field with the decoded block value.
+  Node codecs should also return a `::links` vector."
+  [codec block]
+  (when block
+    (with-open [content (block/open block)]
+      (into block (decode-info! codec content)))))
+
+
+
+;; ## Node Store
 
 (defrecord BlockNodeStore
   [codec store cache]
@@ -24,7 +91,7 @@
         node)
       ; Node is not cached.
       ; TODO: measure parse time
-      (when-let [parsed (node/parse-block codec (block/get store id))]
+      (when-let [parsed (parse-block codec (block/get store id))]
         (let [node (select-keys parsed node/node-keys)]
           (swap! cache cache/miss id node)
           ; TODO: measure cache misses
@@ -33,7 +100,7 @@
 
   (-store-node!
     [this links data]
-    (when-let [block (node/format-block codec {::node/links links, ::node/data data})]
+    (when-let [block (format-block codec {::node/links links, ::node/data data})]
       (let [node (select-keys block node/node-keys)]
         (block/put! store block)
         (swap! cache cache/miss (::node/id node) node)
