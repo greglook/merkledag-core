ca29cb7170a395519bdd85f6e7b1e08891ba1f4d
Greg Look <greg@greg-look.net>
2017-06-07 02:01:09 -0700

    Provide defaults instead of constraints.

--- src/merkledag/system/util.clj
+++ src/merkledag/system/util.clj
@@ -2,16 +2,16 @@
   "Construction functions for component systems."
   (:require
     [blocks.store.memory :refer [memory-block-store]]
-    [merkledag.codec.node-v1 :as v1]
+    [merkledag.codec.node-v1 :refer [edn-node-codec]]
     [merkledag.node.store :refer [block-node-store]]
     [merkledag.node.cache :refer [node-cache]]))
 
 
-(defn memory-store
+(defn init-store
   "Constructs an in-memory node store."
   [& {:as opts}]
   (block-node-store
-    :store (memory-block-store)
-    :codec (v1/edn-node-codec (:types opts))
+    :store (or (:store opts) (memory-block-store))
+    :codec (or (:codec opts) (edn-node-codec (:types opts)))
     :cache (when (:cache opts)
              (atom (apply node-cache {} (apply concat (:cache opts)))))))
